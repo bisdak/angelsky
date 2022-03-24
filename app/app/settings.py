5ef2afe54f0c10d34e92b09c201e5fc407fc8cbd
Kristian Marlowe Ole <krismar.ole@gmail.com>
2022-03-24 17:59:41 +0800

    parsed product describtion

--- app/app/settings.py
+++ app/app/settings.py
@@ -27,7 +27,7 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -68,6 +68,10 @@ ITEM_PIPELINES = {
    'app.pipelines.AppPipeline': 300,
 }
 
+
+FEED_EXPORTERS = {
+    'xlsx': 'scrapy_xlsx.XlsxItemExporter',
+}
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
 #AUTOTHROTTLE_ENABLED = True
--- app/app/spiders/lider_mart_ru.py
+++ app/app/spiders/lider_mart_ru.py
@@ -1,6 +1,8 @@
 import scrapy
 from scrapy.http import JsonRequest
 
+from htmllaundry import strip_markup
+
 from app.items import Product
 
 class LiderMartRuSpider(scrapy.Spider):
@@ -14,12 +16,12 @@ class LiderMartRuSpider(scrapy.Spider):
     def parse_sitemap(self, response):
         response.selector.remove_namespaces()
 
-        for product_url in response.xpath('//url/loc/text()').getall()[:3]:
+        for product_url in response.xpath('//url/loc/text()').getall()[:100]:
             yield scrapy.Request(product_url, callback=self.parse_product)
 
     def parse_description_table(self, response):
         table = {}
-        for tr in table_selector.css('.product_description table.description tr'):
+        for tr in response.css('.product_description table.description tr'):
             key = tr.css('.description_left div::text').get().strip()
             val = tr.css('.description_right div::text').get()
 
@@ -27,19 +29,30 @@ class LiderMartRuSpider(scrapy.Spider):
                 table[key] = val.strip()
         return table
 
-    def parse_barcode(self, response, product):
-        product['barcode'] = response.json()['BarCode']
+    def parse_description_under_tabs(self, response):
+        tabs = response.css('.product_description_full .title li::text').getall()
 
-        yield product
+        table = {}
+        for idx, tab in enumerate(tabs):
+            description = response.css(f'.product_description_full #description_{idx+1}').get()
+            table[tab] = strip_markup(description)
+        return table
 
     def parse_product(self, response):
-        p = Product()
+        description = self.parse_description_table(response)
+        tabs = self.parse_description_under_tabs(response)
 
-        p['title'] = response.css('.product_description h1.title').get()
+        p = Product()
+        p['volume'] = description.get('Объём:', None)
+        p['weight'] = description.get('Вес:', None)
+        p['product_line'] = description.get('Бренд:', None) or description.get('Производитель:', None)
+        p['composition'] = tabs.get('Состав', None)
+
+        p['title'] = response.css('.product_description h1.title::text').get()
         p['price'] = response.css('.product_description [itemprop="price"]::text').get()
         p['currency'] = response.css('meta[itemprop=priceCurrency]::attr(content)').get()
 
-        source = response.url
+        p['source'] = response.url
 
         product_id = response.css('#dataEncryptionProductID::attr(value)').get()
         encryption_key = response.css('#dataEncryptionKey::attr(value)').get()
@@ -52,3 +65,11 @@ class LiderMartRuSpider(scrapy.Spider):
 
         yield scrapy.Request.from_curl(curl_command, callback=self.parse_barcode, 
                                        cb_kwargs=dict(product=p), meta={'dont_redirect': True})
+
+    def parse_barcode(self, response, product):
+        json = response.json()
+
+        product['barcode'] = json.get('Barcode', None)
+        product['article'] = json.get('Article', None)
+
+        yield product
