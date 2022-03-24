f364f6c3959f047245c3a0aef97422e43c5b3481
Kristian Marlowe Ole <krismar.ole@gmail.com>
2022-03-24 16:30:59 +0800

    excluded extra lines from curl headers in lider-mart.ru spider

--- app/app/settings.py
+++ app/app/settings.py
@@ -14,7 +14,9 @@ NEWSPIDER_MODULE = 'app.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'app (+http://www.yourdomain.com)'
+USER_AGENT = ("User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/99.0.4844.82 Safari/537.36")
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
--- app/app/spiders/lider_mart_ru.py
+++ app/app/spiders/lider_mart_ru.py
@@ -46,24 +46,8 @@ class LiderMartRuSpider(scrapy.Spider):
 
         curl_command = (
             "curl 'https://www.lider-mart.ru/Product/GetDataEncryption/' "
-            "-H 'Connection: keep-alive' "
-            "-H 'Pragma: no-cache' "
-            "-H 'Cache-Control: no-cache' "
-            "-H 'sec-ch-ua: \" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"' "
-            "-H 'Accept: */*' "
-            "-H 'X-Requested-With: XMLHttpRequest' "
-            "-H 'sec-ch-ua-mobile: ?0' "
-            "-H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36' "
             "-H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' "
-            "-H 'Origin: https://www.lider-mart.ru' "
-            "-H 'Sec-Fetch-Site: same-origin' "
-            "-H 'Sec-Fetch-Mode: cors' "
-            "-H 'Sec-Fetch-Dest: empty' "
-           f"-H 'Referer: ${source}' "
-            "-H 'Accept-Language: ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7' "
-            "-H 'Cookie: CodeBrowser=3F9708167EB13986C274DA4F26CC6D14; _gid=GA1.2.1523455674.1626008647; _ym_uid=162600864773720386; _ym_d=1626008647; _fbp=fb.1.1626008647643.389622410; _ym_isad=2; _ym_visorc=w; _ga_51FQ6Y1Z7J=GS1.1.1626011558.2.1.1626016725.60; _ga=GA1.2.757362236.1626008647; runid=5013CD59120786E652FBE0B660D5552D; userID=A46C3B54F2C9871CD81DAF7A932499C0' "
            f"--data-raw 'dataEncryptionKey={encryption_key}&productID={product_id}' "
-            "--compressed"
         )
 
         yield scrapy.Request.from_curl(curl_command, callback=self.parse_barcode, 
