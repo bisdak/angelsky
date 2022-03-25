4140eb54ea33bd55e08f18a9e2fe64643a709d14
Kristian Marlowe Ole <krismar.ole@gmail.com>
2022-03-25 13:35:48 +0800

    added product parser for krasota3.ru spider

--- app/app/spiders/krasota3_ru.py
+++ app/app/spiders/krasota3_ru.py
@@ -1,4 +1,6 @@
 import scrapy
+from app.items import Product
+
 
 class Krasota3RuSpider(scrapy.Spider):
     name = 'krasota3.ru'
@@ -8,22 +10,52 @@ class Krasota3RuSpider(scrapy.Spider):
 
 
     def parse(self, response):
+        import ipdb; ipdb.set_trace()
         for category_link in response.css('.product a::attr(href)').getall():
             yield scrapy.Request(response.urljoin(category_link), callback=self.parse_category)
 
-        next_page = response.css('.page-item')[-2]
-        is_diabled_button = bool(next_page.css('.disabled').get())
-        if not is_diabled_button:
-            next_page_link = self.domain + next_page.css('a::attr(href)').get()
-            yield scrapy.Request(next_page_link, callback=self.parse, dont_filter=True)
+        # next_page = response.css('[rel="next"] link::attr(href)').get()
+        
+        # if next_page:
+        #     yield scrapy.Request(next_page, callback=self.parse, dont_filter=True)
 
 
     def parse_category(self, response):
         product_links = response.css('.product-name::attr(href)').getall()
 
         for product_link in product_links:
-            yield scrapy.Request(product_url, callback=self.parse_product)
+            yield scrapy.Request(response.urljoin(product_url), callback=self.parse_product)
 
 
     def parse_product(self, response):
-        pass
+        p = Product()
+
+        p['article'] = response.css('.acticul span::text').get()
+        p['title'] = response.css('h1.heading::text').get()
+        p['product_line'] = response.css('.breadcrumb li span[itemprop=name]::text').getall()[-2]
+
+        p['images'] = self.domain + response.css('#image-block .img-responsive::attr(src)').get()
+
+        p['barcode'] = None
+        p['composition'] = None
+        p['source'] = response.url
+        p['currency'] = None
+
+        description_titles = response.css('.text-content h2::text')
+        for idx, title in enumerate(description_titles):
+            words = title.re('[а-яА-Я]+')
+            words = list(map(lambda w: w.lower(), words))
+
+            if 'состав' in words:
+                try:
+                    p['composition'] = response.css('.text-content p::text').getall()[idx]
+                except IndexError:
+                    pass
+
+        p['weight'] = None
+        for label in response.css('.form-group .flex label'):
+            p['price'] = label.css('span::text').get()
+            p['volume'] = label.css(':not(span) *::text').getall()[-1].strip()
+
+            import ipdb; ipdb.set_trace()
+            yield p
